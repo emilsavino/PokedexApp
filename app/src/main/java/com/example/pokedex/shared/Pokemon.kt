9b777a64cd02cb39552e6edb55ac60cc40de5a2e
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-10-28 19:10:50 +0100

    PokemonGridItem now loads image from url

--- app/src/main/java/com/example/pokedex/shared/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/shared/Pokemon.kt
@@ -2,9 +2,4 @@ package com.example.pokedex.shared
 
 import android.media.Image
 
-class Pokemon(name: String, image: Image?) {
-    var name: String = name
-    var image: Image? = image
-
-
-}
+data class Pokemon(var name: String, var imageURL: String)
--- app/src/main/java/com/example/pokedex/shared/PokemonGridItem.kt
+++ app/src/main/java/com/example/pokedex/shared/PokemonGridItem.kt
@@ -1,27 +1,50 @@
 package com.example.pokedex.shared
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil3.compose.AsyncImage
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.example.pokedex.R
 import java.util.jar.Attributes.Name
 
 @Composable
 fun PokemonGridItem(modifier: Modifier = Modifier, pokemon: Pokemon) {
-    Scaffold { innerPadding ->
-        modifier.padding(innerPadding)
-    }
-}
+        Column()
+        {
+            Text(
+                text = "Pokemon: ${pokemon.name}"
+            )
+            AsyncImage(
+                model = pokemon.imageURL,
+                contentDescription = "Picture of a Pokemon",
+                modifier = Modifier.fillMaxSize(),
+            )
+        }
+
+        }
 
 @Preview(showBackground = true)
 @Composable
 fun PokemonGridItemPreview() {
-    val testPokemon = Pokemon("Pikachu", null)
+    val testPokemon = Pokemon("Pikachu", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png")
     PokemonGridItem(pokemon = testPokemon)
 }

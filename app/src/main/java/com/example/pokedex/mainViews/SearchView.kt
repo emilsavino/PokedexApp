1957cd009a2341b8e09a6aefae07282e68f31512
Mustafa Baker <s235092@dtu.dk>
2024-10-30 14:27:21 +0100

    Created a simple search bar

--- app/src/main/java/com/example/pokedex/mainViews/SearchView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/SearchView.kt
@@ -1,20 +1,47 @@
 package com.example.pokedex.mainViews
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SearchView(modifier: Modifier = Modifier) {
-    Box (
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val searchText = remember { mutableStateOf("") }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFDD99)),
+        contentAlignment = Alignment.TopCenter,
     ) {
-        Text("Search View")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            TextField(
+                value = searchText.value,
+                onValueChange = { searchText.value = it },
+                placeholder = { Text("Search...") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent),
+                shape = RoundedCornerShape(24.dp),
+                maxLines = 1
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
     }
 }
 

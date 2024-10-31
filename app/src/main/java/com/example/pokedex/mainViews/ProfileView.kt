51e61ffacf97b833c29ecf680539bf2371b6bba9
Mads Hartwich <s235102@dtu.dk>
2024-10-31 13:23:10 +0100

    profile

--- app/src/main/java/com/example/pokedex/mainViews/ProfileView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/ProfileView.kt
@@ -1,17 +1,21 @@
 package com.example.pokedex.mainViews
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ProfileView(modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
         contentAlignment = Alignment.Center
     ) {
         Text("Profile View")

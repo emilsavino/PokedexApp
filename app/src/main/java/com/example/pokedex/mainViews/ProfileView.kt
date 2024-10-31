0bc1f9159073e4d6dea82f0ef237499471ba0627
Mads Hartwich <s235102@dtu.dk>
2024-10-31 15:48:24 +0100

    added change password, sign out, and delete account buttons

--- app/src/main/java/com/example/pokedex/mainViews/ProfileView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/ProfileView.kt
@@ -2,13 +2,19 @@ package com.example.pokedex.mainViews
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfileView(modifier: Modifier = Modifier) {
@@ -18,8 +24,45 @@ fun ProfileView(modifier: Modifier = Modifier) {
             .background(Color.LightGray),
         contentAlignment = Alignment.Center
     ) {
-        Text("Profile View")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ){
+        Button(
+            onClick = { /*Add button functionality*/},
+
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Gray,
+                contentColor = Color.Black
+            )
+        ) {
+            Text(text = "Change Password")
+        }
+
+        Button(
+            onClick = { /*Add button functionality*/},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Gray,
+                contentColor = Color.Red
+            )
+        ) {
+            Text(text = "Sign Out")
+        }
+
+            Button(
+                onClick = { /*Add button functionality*/},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray,
+                    contentColor = Color.Red
+                )
+            ) {
+                Text(text = "Delete Account")
+            }
+
+        }
     }
+
 }
 
 @Preview(showBackground = true)

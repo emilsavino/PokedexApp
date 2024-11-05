2567a315da325cb0a632a3d303c276d6a6d014dd
Mads Hartwich <s235102@dtu.dk>
2024-11-05 11:36:53 +0100

    created buttons for profileView

--- app/src/main/java/com/example/pokedex/mainViews/ProfileView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/ProfileView.kt
@@ -1,15 +1,23 @@
 package com.example.pokedex.mainViews
 
+import android.view.Display.Mode
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,7 +25,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
+fun ProfileButton(
+    text: String,
+    onClick: () -> Unit,
+    containerColor: Color = Color.Gray,
+    contentColor: Color = Color.Black
+){
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Gray,
+            contentColor = Color.Black
+        ),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = text)
+    }
+
+}
+
+@Composable
 fun ProfileView(modifier: Modifier = Modifier) {
+
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -29,36 +58,40 @@ fun ProfileView(modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .padding(16.dp)
         ){
-        Button(
-            onClick = { /*Add button functionality*/},
+            Text("Profile Picture")
 
-            colors = ButtonDefaults.buttonColors(
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("name@dtu.dk")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ProfileButton(
+                text = "Change Password",
+                onClick = { /* Add button functionality */ },
                 containerColor = Color.Gray,
                 contentColor = Color.Black
             )
-        ) {
-            Text(text = "Change Password")
-        }
 
-        Button(
-            onClick = { /*Add button functionality*/},
-            colors = ButtonDefaults.buttonColors(
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ProfileButton(
+                text = "Sign Out",
+                onClick = { /* Add button functionality */ },
                 containerColor = Color.Gray,
                 contentColor = Color.Red
             )
-        ) {
-            Text(text = "Sign Out")
-        }
 
-            Button(
-                onClick = { /*Add button functionality*/},
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Gray,
-                    contentColor = Color.Red
-                )
-            ) {
-                Text(text = "Delete Account")
-            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ProfileButton(
+                text = "Delete Account",
+                onClick = { /* Add button functionality */ },
+                containerColor = Color.Gray,
+                contentColor = Color.Red
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
 
         }
     }

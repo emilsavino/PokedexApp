0daae2bd9a13855a085ea70639d8a1286bc98b85
Frederik Bastrup <s235117@dtu.dk>
2024-11-06 12:37:47 +0100

    Added composables for pokemon of the day, whos that pokemon and pokemon trivia

--- app/src/main/java/com/example/pokedex/mainViews/HomeView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/HomeView.kt
@@ -1,25 +1,64 @@
 package com.example.pokedex.mainViews
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.ImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.example.pokedex.data.MockPokemonDataStore
+import com.example.pokedex.shared.Pokemon
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeView(modifier: Modifier = Modifier) {
-    PokemonOfTheDayView(modifier)
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopCenter
+
+    val coroutineScope = rememberCoroutineScope()
+    var pokemonList by remember { mutableStateOf<List<Pokemon>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            pokemonList = MockPokemonDataStore().fetchPokemons()
+        }
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
     ) {
-        Text(
-            text = "Home View",
-            style = MaterialTheme.typography.titleLarge,
-        )
+        if(pokemonList.isNotEmpty()) {
+            PokemonOfDayView(pokemon = pokemonList[0])
+        }
+        GamesRow()
     }
 }
 
@@ -31,23 +70,81 @@ fun HomeViewPreview() {
 }
 
 @Composable
-fun PokemonOfTheDayView(modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+fun PokemonOfDayView(pokemon: Pokemon) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Who's that Pokemon?")
+        Text(
+            text = "Pokémon of the Day",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Image(
+            painter = rememberAsyncImagePainter(model = pokemon.imageURL),
+            contentDescription = pokemon.name,
+            modifier = Modifier.size(120.dp),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Name: ", fontSize = 16.sp
+            )
+            Text(
+                text = pokemon.name, fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = "Type: ", fontSize = 16.sp
+            )
+            Text(
+                //TODO: type needs to be updated so that it can be displayed as the correct type.
+                text = "Fire", fontSize = 16.sp
+            )
+        }
     }
 }
 
-fun OptionsRowView() {
+@Composable
+fun GamesRow() {
+    Row(
+        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Box(
+            modifier = Modifier
+                .width(150.dp)
+                .height(120.dp)
+                .background(MaterialTheme.colorScheme.primary, RoundedCornerShape(8.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Who's that Pokémon?", fontSize = 14.sp)
+        }
+        Box(
+            modifier = Modifier
+                .width(150.dp)
+                .height(120.dp)
+                .background(
+                    MaterialTheme.colorScheme.secondary, RoundedCornerShape(8.dp)
+                ), contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Pokémon Trivia", fontSize = 14.sp)
+        }
+    }
+}
+
+@Composable
+fun RecentlyViewedPokemonView(recentPokemons: List<Pokemon>) {
 
 }
 
-fun RecentlyViewedView() {
-
-}
-
+@Composable
 fun RecentlyViewedPokemon() {
 
 }

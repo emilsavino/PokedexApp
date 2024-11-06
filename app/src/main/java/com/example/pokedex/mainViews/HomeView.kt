bd927bf9ffae0fc0dca61d01519511fe3b6cdfe3
Frederik Bastrup <s235117@dtu.dk>
2024-11-06 12:00:49 +0100

    Added functions to the HomeView for all the different parts of the view

--- app/src/main/java/com/example/pokedex/mainViews/HomeView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/HomeView.kt
@@ -2,6 +2,7 @@ package com.example.pokedex.mainViews
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,16 +11,40 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeView(modifier: Modifier = Modifier) {
+    PokemonOfTheDayView(modifier)
     Box(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.TopCenter
     ) {
-        Text("Home View")
+        Text(
+            text = "Home View",
+            style = MaterialTheme.typography.titleLarge,
+        )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun HomeViewPreview() {
     HomeView()
 }
+
+@Composable
+fun PokemonOfTheDayView(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Who's that Pokemon?")
+    }
+}
+
+fun OptionsRowView() {
+
+}
+
+fun RecentlyViewedView() {
+
+}
+

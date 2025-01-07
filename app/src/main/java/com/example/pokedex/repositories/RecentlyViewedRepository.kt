8568a4843ed063c80b9e89979f34d9604a84d7ca
emilsavino <s235096@dtu.dk>
2025-01-07 08:35:19 +0100

    creared repo for recently viewed

---
+++ app/src/main/java/com/example/pokedex/repositories/RecentlyViewedRepository.kt
@@ -0,0 +1,73 @@
+package com.example.pokedex.repositories
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.pokedex.shared.Pokemon
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+private val Context.dataStore by preferencesDataStore(name = "recently_viewed_preferences")
+
+class RecentlyViewedRepository(private val context: Context) {
+    private val recentlyViewedPokemons = mutableListOf<Pokemon>()
+    private val RECENTLY_VIEWED_KEY = stringPreferencesKey("recently_viewed")
+    private val gson = Gson()
+
+    private val mutableRecentlyViewedPokemonsFlow = MutableSharedFlow<List<Pokemon>>()
+    val recentlyViewedPokemonsFlow: Flow<List<Pokemon>> = mutableRecentlyViewedPokemonsFlow.asSharedFlow()
+
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            initializeCache()
+        }
+    }
+
+    suspend fun initializeCache() {
+        val savedPokemons = fetchRecentsFromDataStore()
+        recentlyViewedPokemons.clear()
+        recentlyViewedPokemons.addAll(savedPokemons)
+    }
+
+    suspend fun fetchRecents() {
+        mutableRecentlyViewedPokemonsFlow.emit(recentlyViewedPokemons)
+    }
+
+    suspend fun addToRecentsFavourite(pokemon: Pokemon) {
+        if (!recentlyViewedPokemons.contains(pokemon)) {
+            recentlyViewedPokemons.add(pokemon)
+            updateDataStore()
+        }
+    }
+
+    suspend fun removeFromRecents(pokemon: Pokemon) {
+        if (recentlyViewedPokemons.contains(pokemon)) {
+            recentlyViewedPokemons.remove(pokemon)
+            updateDataStore()
+        }
+    }
+
+    fun pokemonIsRecent(pokemon: Pokemon): Boolean {
+        return recentlyViewedPokemons.contains(pokemon)
+    }
+
+    private suspend fun updateDataStore() {
+        val pokemonJson = gson.toJson(recentlyViewedPokemons)
+        context.dataStore.edit { preferences ->
+            preferences[RECENTLY_VIEWED_KEY] = pokemonJson
+        }
+    }
+
+    private suspend fun fetchRecentsFromDataStore(): List<Pokemon> {
+        val preferences = context.dataStore.data.first()
+        val pokemonJson = preferences[RECENTLY_VIEWED_KEY] ?: "[]"
+        return gson.fromJson(pokemonJson, Array<Pokemon>::class.java).toList()
+    }
+}

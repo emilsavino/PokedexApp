2b43b52bb8e2ce17f32572e076d51479149068e7
Mustafa <s235092@dtu.dk>
2025-01-07 10:31:28 +0100

    Better tab bar

--- app/src/main/java/com/example/pokedex/navigation/TabBar.kt
+++ app/src/main/java/com/example/pokedex/navigation/TabBar.kt
@@ -1,16 +1,26 @@
 package com.example.pokedex.navigation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.pokedex.R
+
 
 @Composable
 fun TabBar(navController: NavController) {
@@ -22,16 +32,54 @@ fun TabBar(navController: NavController) {
                 selected = index == viewModel.selectedTabIndex,
                 onClick = {
                     viewModel.selectedTabIndex = index
-                    navController.navigate(tab.first)
+                    navController.navigate(tab.route)
                 },
                 icon = {
-                    Text(
-                        tab.second,
-                        modifier = Modifier.padding(vertical = 8.dp)
-                    )
+                    if (tab.title == "Home") {
+                        CreateHomeButton(tab)
+                    } else {
+                        CreateOtherButtons(tab)
+                    }
                 }
             )
         }
+    }
+}
+
+@Composable
+fun CreateHomeButton(tab: Tab) {
+    Column(
+        modifier = Modifier.padding(2.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.pokeball),
+            contentDescription = "Home",
+            modifier = Modifier
+                .size(25.dp)
+        )
+        Text(
+            text = tab.title,
+            fontSize = 12.sp,
+        )
+    }
+}
+
+@Composable
+fun CreateOtherButtons(tab: Tab) {
+    Column (
+        modifier = Modifier.padding(2.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Icon(
+            imageVector = tab.icon,
+            contentDescription = "${tab.title} Icon",
+            tint = Color.Black
+        )
+        Text(
+            text = tab.title,
+            fontSize = 12.sp
+        )
     }
 }
 
--- app/src/main/java/com/example/pokedex/navigation/TabBarViewModel.kt
+++ app/src/main/java/com/example/pokedex/navigation/TabBarViewModel.kt
@@ -1,17 +1,30 @@
 package com.example.pokedex.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.ViewModel
 
 class TabBarViewModel: ViewModel() {
     var selectedTabIndex by mutableIntStateOf(2)
     val tabs = listOf(
-        Screen.Saved.route to "Saved",
-        Screen.MyTeams.route to "Teams",
-        Screen.Home.route to "Home",
-        Screen.Search.route to "Search",
-        Screen.Profile.route to "Profile"
+        Tab(route = Screen.Saved.route, title = "Saved", icon = Icons.Outlined.FavoriteBorder),
+        Tab(route = Screen.MyTeams.route, title = "Teams", icon = Icons.Outlined.List),
+        Tab(route = Screen.Home.route, title = "Home", icon = Icons.Outlined.Home),
+        Tab(route = Screen.Search.route, title = "Search", icon = Icons.Outlined.Search),
+        Tab(route = Screen.Profile.route, title = "Profile", icon = Icons.Outlined.Person)
     )
 }
+
+data class Tab(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+)

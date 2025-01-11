89bce7a04467c9b3a2712a147612ce7ead209033
emilsavino <s235096@dtu.dk>
2025-01-11 10:47:22 +0100

    Saved now uses UI states

--- app/src/main/java/com/example/pokedex/mainViews/SavedPokemonsView/SavedView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/SavedPokemonsView/SavedView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,46 +19,68 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.pokedex.shared.Pokemon
 import com.example.pokedex.shared.PokemonGridItem
+import com.example.pokedex.shared.ProgressIndicator
 
 @Composable
-fun SavedView(modifier: Modifier = Modifier, navController: NavController) {
-    val viewModel = SavedViewModel()
+fun SavedView(navController: NavController) {
+    val viewModel = viewModel<SavedViewModel>()
+    val savedState = viewModel.savedState.collectAsState().value
+
     Column(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFFFDD99)),
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .background(Color(0xFFFFDD99))
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Saved",
-            fontSize = 60.sp,
-            modifier = modifier.padding(10.dp)
+            text = "Saved Pokemons",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(8.dp),
         )
-        SavedList(viewModel, navController)
-        Spacer(modifier = Modifier.padding(10.dp))
-    }
 
+        when (savedState) {
+            is SavedUIState.Empty -> {
+                Text(text = "No saved pokemons")
+            }
+
+            is SavedUIState.Loading -> {
+                ProgressIndicator()
+            }
+
+            is SavedUIState.Data -> {
+                if (savedState.saved.isEmpty()) {
+                    Text(text = "No saved pokemons")
+                    viewModel.savedIsEmpty()
+                } else {
+                    SavedList(navController, savedState.saved)
+                }
+            }
+        }
+    }
 }
+
 @Composable
-fun SavedList (savedViewModel: SavedViewModel, navController: NavController) {
-    val pokemons = savedViewModel.savedState.collectAsState().value
-    val pokemonsChunked = pokemons.chunked(3)
+private fun SavedList (navController: NavController, savedPokemons: List<Pokemon>) {
+    val pokemonsChunked = savedPokemons.chunked(3)
     LazyColumn (
         modifier = Modifier,
         verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
-        items(pokemonsChunked) { item: List<Pokemon> ->
+        items(pokemonsChunked) { item ->
             SavedRow(pokemons = item, navController = navController)
         }
     }
 }
 
 @Composable
-fun SavedRow(modifier: Modifier = Modifier, pokemons : List<Pokemon>, navController: NavController) {
+private fun SavedRow(modifier: Modifier = Modifier, pokemons : List<Pokemon>, navController: NavController) {
     Row (
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(19.dp)

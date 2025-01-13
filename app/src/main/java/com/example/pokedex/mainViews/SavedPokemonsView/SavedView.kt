504f6a428ad0b2ebde3effa56909fce938f0c12d
Mustafa <s235092@dtu.dk>
2025-01-13 23:41:46 +0100

    Text size seemed too small in saved

--- app/src/main/java/com/example/pokedex/mainViews/SavedPokemonsView/SavedView.kt
+++ app/src/main/java/com/example/pokedex/mainViews/SavedPokemonsView/SavedView.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,6 +14,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,7 +40,8 @@ fun SavedView(navController: NavController) {
     ) {
         Text(
             text = "Saved Pokemons",
-            fontSize = 24.sp,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(8.dp),
         )
 

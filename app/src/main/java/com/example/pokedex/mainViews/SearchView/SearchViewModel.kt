3bbfc9fce0112672d58c4d5b2c28d516a22236d5
emilsavino <s235096@dtu.dk>
2025-01-15 21:10:08 +0100

    merged with main again...

--- app/src/main/java/com/example/pokedex/mainViews/SearchView/SearchViewModel.kt
+++ app/src/main/java/com/example/pokedex/mainViews/SearchView/SearchViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.pokedex.mainViews.SearchView
 
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope

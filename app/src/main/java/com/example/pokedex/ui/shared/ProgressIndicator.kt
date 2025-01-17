3e735ac24790e1de37e0e32a2afabd6cf59d3286
Elias Aggergaard Larsen <s235125@dtu.dk>
2025-01-17 02:04:15 +0100

    Extracted loading screen from SplashView to ProgressIndicator

--- app/src/main/java/com/example/pokedex/ui/shared/ProgressIndicator.kt
+++ app/src/main/java/com/example/pokedex/ui/shared/ProgressIndicator.kt
@@ -1,11 +1,17 @@
 package com.example.pokedex.ui.shared
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.pokedex.R
 
 @Composable
 fun ProgressIndicator() {
@@ -13,6 +19,17 @@ fun ProgressIndicator() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        Image(
+            painter = painterResource(id = R.drawable.loading_screen),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        CircularProgressIndicator(
+            color = Color.Blue,
+            strokeWidth = 4.dp
+        )
     }
 }
+
+

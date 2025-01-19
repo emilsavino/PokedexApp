f677f3860ea84de28611c239259adaad78e50f9d
Mads Hartwich <s235102@dtu.dk>
2025-01-19 12:10:12 +0100

    fixed scroll bars

--- app/src/main/java/com/example/pokedex/ui/mainViews/SearchView/SearchView.kt
+++ app/src/main/java/com/example/pokedex/ui/mainViews/SearchView/SearchView.kt
@@ -1,13 +1,12 @@
 package com.example.pokedex.mainViews.SearchView
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,6 +14,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
@@ -22,8 +22,10 @@ import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,22 +33,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.example.pokedex.R
-import com.example.pokedex.ui.navigation.Screen
 import com.example.pokedex.dataClasses.formatPokemonName
 import com.example.pokedex.dataClasses.Pokemon
 import com.example.pokedex.dataClasses.PokemonTypeResources
@@ -59,7 +54,8 @@ fun SearchView(
     modifier: Modifier = Modifier,
     navController: NavController,
     filterOption: String,
-    viewModel: SearchViewModel = viewModel()
+    viewModel: SearchViewModel = viewModel(),
+    dismiss: (() -> Unit)? = null
 ) {
     val scrollState : ScrollState = rememberScrollState()
     val pokemons = viewModel.pokemonList.collectAsState().value
@@ -79,6 +75,12 @@ fun SearchView(
             }
     }
 
+    BackHandler {
+        if (dismiss != null) {
+            dismiss()
+        }
+    }
+
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -91,7 +93,8 @@ fun SearchView(
                 .padding(16.dp)
                 .verticalScroll(scrollState)
         ) {
-            MakeSearchTools(viewModel = viewModel)
+
+            MakeSearchTools(viewModel, dismiss)
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -121,8 +124,8 @@ fun SearchView(
 }
 
 @Composable
-private fun MakeSearchTools(viewModel: SearchViewModel) {
-    MakeSearchBar(viewModel)
+private fun MakeSearchTools(viewModel: SearchViewModel, dismiss: (() -> Unit)?) {
+    MakeSearchBar(viewModel, dismiss)
 
     Spacer(modifier = Modifier.height(8.dp))
 
@@ -144,36 +147,61 @@ private fun MakeSearchTools(viewModel: SearchViewModel) {
 }
 
 @Composable
-private fun MakeSearchBar(viewModel: SearchViewModel) {
-    OutlinedTextField(
+private fun MakeSearchBar(viewModel: SearchViewModel, dismiss: (() -> Unit)?) {
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(24.dp))
             .background(Color.White)
-            .shadow(0.dp),
+            .shadow(0.dp)
+            .border(1.dp, Color.Black, RoundedCornerShape(24.dp)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (dismiss != null) {
+            Box(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .size(20.dp)
+            ) {
+                IconButton(
+                    onClick = { dismiss() },
+                    modifier = Modifier
+                        .size(20.dp)
+                ) {
+                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                }
+            }
+        }
 
-        placeholder = { Text(
-            "Search...",
-            color = Color.Black
-        ) },
-
-        onValueChange = {
-            viewModel.searchText.value = it
-            viewModel.searchPokemonList()
-        },
-
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Text,
-            imeAction = ImeAction.Done,
-            autoCorrectEnabled = false
-        ),
-
-        keyboardActions = KeyboardActions.Default,
-        value = viewModel.searchText.value,
-        shape = RoundedCornerShape(24.dp),
-        singleLine = true,
-    )
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(
+                    "Search...",
+                    color = Color.Black
+                )
+            },
+            onValueChange = {
+                viewModel.searchText.value = it
+                viewModel.searchPokemonList()
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done,
+                autoCorrectEnabled = false
+            ),
+            keyboardActions = KeyboardActions.Default,
+            value = viewModel.searchText.value,
+            shape = RoundedCornerShape(24.dp),
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent
+            )
+        )
+    }
 }
+
 
 @Composable
 private fun MakeFilterButton(
@@ -183,79 +211,64 @@ private fun MakeFilterButton(
     unselectedColor: Color
 ) {
     val typeResources = PokemonTypeResources()
-    var filterExpanded by remember { mutableStateOf(false) }
+    var filterExpanded = remember { mutableStateOf(false) }
+    var maxVisibleItems = 5
     val scrollState = rememberScrollState()
-    val maxVisibleItems = 5
-
-    Box(
-        contentAlignment = Alignment.TopStart,
-        modifier = Modifier.wrapContentSize()
+    Button(
+        onClick = { filterExpanded.value = true},
+        colors = buttonColors(containerColor = Color.White),
+        modifier = Modifier.shadow(2.dp, CircleShape)
     ) {
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                imageVector = Icons.Filled.List,
+                contentDescription = "Filter",
+                tint = textColor
+            )
+            Spacer(modifier = Modifier.padding(2.dp))
 
-        Button(
-            onClick = { filterExpanded = !filterExpanded },
-            colors = buttonColors(containerColor = Color.White),
-            modifier = Modifier.shadow(2.dp, CircleShape)
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.List,
-                    contentDescription = "Filter",
-                    tint = textColor
-                )
-                Spacer(modifier = Modifier.padding(2.dp))
-                Text(
-                    text = "Filter",
-                    color = Color.Black
-                )
-            }
+            Text(
+                text = "Filter",
+                color = Color.Black
+            )
         }
 
-        if (filterExpanded) {
-            Box(
-                modifier = Modifier
-                    .offset(y = 56.dp)
-                    .width(IntrinsicSize.Min)
-                    .background(Color.White, shape = RoundedCornerShape(8.dp))
-                    .shadow(4.dp, shape = RoundedCornerShape(8.dp))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .heightIn(max = (maxVisibleItems * 48).dp)
-                        .verticalScrollbar(
-                            scrollState = scrollState,
-                            showScrollBarTrack = false,
-                            scrollBarColor = Color.Gray
-                        )
-                        .verticalScroll(scrollState)
-                ) {
-                    val allFilterOptions = viewModel.getAllFilterOptions()
-                    for (option in allFilterOptions) {
-                        DropdownMenuItem(
-                            text = {
-                                Row(verticalAlignment = Alignment.CenterVertically) {
-                                    Image(
-                                        painter = typeResources.getTypeImage(option),
-                                        contentDescription = "${option.capitalize()} type icon",
-                                        modifier = Modifier.size(24.dp).padding(end = 8.dp)
-                                    )
-                                    Text(option.capitalize(), color = textColor)
-                                }
-                            },
-                            modifier = Modifier.background(
-                                color = if (viewModel.selectedFilterOptionsList.value.contains(option))
-                                    selectedColor else unselectedColor
-                            ),
-                            onClick = {
-                                viewModel.selectFilterOption(option)
-                                filterExpanded = false
-                            }
-                        )
+        DropdownMenu(
+            expanded = filterExpanded.value,
+            onDismissRequest = { filterExpanded.value = false },
+            modifier = Modifier
+                .height((maxVisibleItems * 48).dp)
+                .background(Color.White)
+                .verticalScrollbar(
+                    scrollState = scrollState,
+                    showScrollBarTrack = false,
+                    scrollBarColor = Color.Gray
+                ),
+            scrollState = scrollState
+        ) {
+            val allFilterOptions = viewModel.getAllFilterOptions()
+            for (option in allFilterOptions) {
+                DropdownMenuItem(
+                    text = {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Image(
+                                painter = typeResources.getTypeImage(option),
+                                contentDescription = "${option.capitalize()} type icon",
+                                modifier = Modifier.size(24.dp).padding(end = 8.dp)
+                            )
+                            Text(option.capitalize(), color = textColor)
+                        }
+                    },
+                    modifier = Modifier.background(
+                        color = if (viewModel.selectedFilterOptionsList.value.contains(option)) selectedColor else unselectedColor
+                    ),
+                    onClick = {
+                        viewModel.selectFilterOption(option)
                     }
-                }
+                )
             }
         }
     }
@@ -268,71 +281,56 @@ private fun MakeSortButton(
     selectedColor: Color,
     unselectedColor: Color
 ) {
-    var sortExpanded by remember { mutableStateOf(false) }
+    var sortExpanded = remember { mutableStateOf(false) }
+    val maxVisibleItems = 5
     val scrollState = rememberScrollState()
-    val maxVisibleItems = 3
-
-    Box(
-        contentAlignment = Alignment.TopStart,
-        modifier = Modifier.wrapContentSize()
+    Button(
+        onClick = { sortExpanded.value = true },
+        colors = buttonColors(containerColor = Color.White),
+        modifier = Modifier.shadow(2.dp, CircleShape)
     ) {
-
-        Button(
-            onClick = { sortExpanded = !sortExpanded },
-            colors = buttonColors(containerColor = Color.White),
-            modifier = Modifier.shadow(2.dp, CircleShape)
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.MoreVert,
-                    contentDescription = "Sort",
-                    tint = textColor
-                )
-                Spacer(modifier = Modifier.padding(2.dp))
-                Text(
-                    text = "Sort",
-                    color = Color.Black
-                )
-            }
+            Icon(
+                imageVector = Icons.Filled.MoreVert,
+                contentDescription = "Sort",
+                tint = textColor
+            )
+            Spacer(modifier = Modifier.padding(2.dp))
+
+            Text(
+                text = "Sort",
+                color = Color.Black
+            )
         }
 
-        if (sortExpanded) {
-            Box(
-                modifier = Modifier
-                    .offset(y = 56.dp)
-                    .width(IntrinsicSize.Min)
-                    .background(Color.White, shape = RoundedCornerShape(8.dp))
-                    .shadow(4.dp, shape = RoundedCornerShape(8.dp))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .heightIn(max = (maxVisibleItems * 48).dp)
-                        .verticalScrollbar(
-                            scrollState = scrollState,
-                            showScrollBarTrack = false,
-                            scrollBarColor = Color.Gray
-                        )
-                        .verticalScroll(scrollState)
-                ) {
-                    val allSortOptions = viewModel.getAllSortOptions()
-                    for (option in allSortOptions) {
-                        DropdownMenuItem(
-                            text = {
-                                Text(option, color = textColor)
-                            },
-                            modifier = Modifier.background(
-                                color = if (viewModel.selectedSortOption.value == option) selectedColor else unselectedColor
-                            ),
-                            onClick = {
-                                viewModel.selectSortOption(option)
-                                sortExpanded = false
-                            }
-                        )
+        DropdownMenu(
+            expanded = sortExpanded.value,
+            onDismissRequest = { sortExpanded.value = false },
+            modifier = Modifier
+                .height((maxVisibleItems * 48).dp)
+                .background(Color.White)
+                .verticalScrollbar(
+                    scrollState = scrollState,
+                    showScrollBarTrack = false,
+                    scrollBarColor = Color.Gray
+                ),
+            scrollState = scrollState
+        ) {
+            val allSortOptions = viewModel.getAllSortOptions()
+            for (option in allSortOptions) {
+                DropdownMenuItem(
+                    text = { Text(option, color = textColor) },
+                    modifier = Modifier.background(
+                        color = if (viewModel.selectedSortOption.value == option) selectedColor else unselectedColor
+                    ),
+                    onClick = {
+                        sortExpanded.value = false
+                        viewModel.selectSortOption(option)
                     }
-                }
+                )
             }
         }
     }
